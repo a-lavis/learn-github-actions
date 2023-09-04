63df4fdd3b2b536deae9433763cbb7bb0bc44480
Aidan <a.lavis@kickstarter.com>
2023-09-04 13:31:04 -0400

    try this

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 
 gem "standard", "~> 1.3"
+
+gem "mysql2"
--- spec/learn/github/actions_spec.rb
+++ spec/learn/github/actions_spec.rb
@@ -5,7 +5,33 @@ RSpec.describe Learn::Github::Actions do
     expect(Learn::Github::Actions::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let :user do
+    ENV.fetch("TEST_DB_USER")
+  end
+
+  let :client do
+    Mysql2::Client.new(
+      host: ENV.fetch("TEST_DB_HOST"),
+      username: user,
+      password: ENV.fetch("TEST_DB_PASS")
+    )
+  end
+
+  describe "#query" do
+    let :query do
+      client.query("SELECT CURRENT_USER() AS user")
+    end
+
+    it "return a Mysql2::Result" do
+      expect(query).to be_instance_of(Mysql2::Result)
+    end
+
+    it "returns a result of size 1" do
+      expect(query.to_a.size).to eq(1)
+    end
+
+    it "returns the correct result" do
+      expect(query.to_a.first.first).to match(/^#{user}@.+$/)
+    end
   end
 end
